7152a64a4f77788d200432ebdcf269d2c809d074
Hamza <muhammadmhamza35@gmail.com>
2025-04-01 15:40:26 -0400

    completed exercies 1-3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(message) {
+reversedMessage="";
+for(i=message.length-1; i>=0; i--){
+    reversedMessage+=message[i];
+}
+return reversedMessage;
 };
 
 // Do not edit below this line
