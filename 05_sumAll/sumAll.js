c7c269b821b3a6d80846b8b464beac288c3878a3
Hamza <muhammadmhamza35@gmail.com>
2025-04-03 13:53:10 -0400

    Finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
 
-};
+let sum = 0;
+let i = 0;
+let x = 0;
+if(Number.isInteger(numOne) && Number.isInteger(numTwo)){
+  if(numOne > 0 && numTwo >0){
+    if(numOne < numTwo){
+      i = numOne;
+      x = numTwo;
+    }
+    else{
+      i = numTwo;
+      x = numOne;
+    }
+
+    while(i<=x){
+      sum=sum+i;
+      i++;
+    }
+
+    return sum;
+  }
+  else  
+    return 'ERROR';
+}
+else
+  return 'ERROR';
+
+}
 
 // Do not edit below this line
 module.exports = sumAll;
