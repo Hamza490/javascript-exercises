e0d7c30a47488e932cfc2b4fce74577a5a495047
Hamza <muhammadmhamza35@gmail.com>
2025-04-02 21:32:58 -0400

    finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(inputArray,...nums) {
 
+  let removedIndeces = [];
+  let i=0;
+
+  for(let element of inputArray){
+  if(!nums.includes(element)){
+    removedIndeces.push(element);
+  }
+  i++;
+  }
+
+  return removedIndeces;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
