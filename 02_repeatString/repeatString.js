0711225d3c5bd3a5d7abf9c7d6c9c674bd31620d
Hamza <muhammadmhamza35@gmail.com>
2025-04-03 14:31:47 -0400

    staged and committing exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(message, num) {
+if(num<0)
+    return "ERROR";
+else{
+let concatenatedMessage="";
+  for(i=0; i<num; i++){
+    concatenatedMessage+=message;
+}
 
+return concatenatedMessage;
+}
 };
 
 // Do not edit below this line
